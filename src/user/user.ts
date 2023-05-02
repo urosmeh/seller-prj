efdbb2c2f59f98747bbec5351353017f1a7a1e52
Uroš Meh <uros.meh@pro-bit.si>
2023-05-02 16:36:38 +0200

    initial user module and entity

---
+++ src/user/user.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name: 'users',
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: true })
+  isSeller: boolean;
+}
